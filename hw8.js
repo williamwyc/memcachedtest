ee2396c3d855e422b2759ff7546c86034ce8b825
Yichun Wu <kobo1998@live.com>
2019-11-06 10:09:56 -0500

    memcached

--- hw8.js
+++ hw8.js
@@ -1,41 +1,60 @@
 var express = require('express');
 var router = express.Router();
-var utf8 = require('utf8');
+var Memcached = require('memcached');
+
+var memcached = new Memcached('localhost:11211');
 
 router.get('/',function(req,res){
     db = req.app.locals.db
     data = req.query
     club = data.club
     pos = data.pos
-    db.query('SELECT Player, GS, A FROM assists WHERE Club =\"'+club+'\"and Pos =\"'+pos+"\"",function(err,result,fields){
+    key = club+pos
+    memcached.get(key,function(err,data){
         if(err){
             console.log(err)
         }
+        else if(data!=null){
+            res.json(data)
+        }
         else{
-            var player = ''
-            var goal = -1
-            var assist = -1
-            var total = 0
-            var count = result.length
-            for(var i = 0; i<result.length; i++){
-                var p = result[i]
-                total += p.A
-                if(p.A > assist || (p.A == assist && p.GS > goal)){
-                    assist = p.A
-                    goal = p.GS
-                    player = p.Player
+            db.query('SELECT Player, GS, A FROM assists WHERE Club =\"'+club+'\"and Pos =\"'+pos+"\"",function(err,result,fields){
+                if(err){
+                    console.log(err)
                 }
-            }
-            var avg = total/count
-            json = {
-                'club': club,
-                'pos': pos,
-                'max_assists': assist,
-                'player': player.toString('utf8'),
-                'avg_assists': avg
-            }
-            console.log(json)
-            res.json(json)
+                else{
+                    var player = ''
+                    var goal = -1
+                    var assist = -1
+                    var total = 0
+                    var count = result.length
+                    for(var i = 0; i<result.length; i++){
+                        var p = result[i]
+                        total += p.A
+                        if(p.A > assist || (p.A == assist && p.GS > goal)){
+                            assist = p.A
+                            goal = p.GS
+                            player = p.Player
+                        }
+                    }
+                    var avg = total/count
+                    json = {
+                        'club': club,
+                        'pos': pos,
+                        'max_assists': assist,
+                        'player': player.toString('utf8'),
+                        'avg_assists': avg
+                    }
+                    memcached(key,json,5000,function(err){
+                        if(err){
+                            console.log(err)
+                        }
+                        else{
+                            res.json(json)
+                        }
+                    })
+                }
+            })
         }
     })
 })
